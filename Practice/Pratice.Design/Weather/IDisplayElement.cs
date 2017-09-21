c89cce0334ef01b097b0369f9d296042116b6cea
chengongyi <chengongyi@qq.com>
2017-09-22 00:14:01 +0800

    气象站的框架搭建

---
+++ Practice/Pratice.Design/Weather/IDisplayElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratice.Design.Weather
+{
+    /// <summary>
+    /// 展示接口
+    /// </summary>
+    public interface IDisplayElement
+    {
+        /*
+         * 这个接口就和业务有关联了，
+         * 因为这里是未来扩展的地方，后面的新加入的公告版。
+         */
+        void Display();
+    }
+}
---
+++ Practice/Pratice.Design/Weather/Observer.cs
@@ -0,0 +1,14 @@
+﻿namespace Pratice.Design.Weather
+{
+    /// <summary>
+    /// 观察者
+    /// </summary>
+    public interface IObserver
+    {
+        /*
+         * 想要成为观察者，则必须实现这个接口，
+         * 因为成为观察者，是需要条件的
+         */
+        void Update(float temp, float humidity, float pressure);
+    }
+}
