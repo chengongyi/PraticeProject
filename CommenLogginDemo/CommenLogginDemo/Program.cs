2bd0f3948452c5897e59b22c289ccd1fed859f0e
chengongyi <chengongyi@email.com>
2018-05-23 14:00:35 +0800

    【add】 学习了最简单的CommenLogging

---
+++ CommenLogginDemo/CommenLogginDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging; 
+
+namespace CommenLogginDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog log = LogManager.GetLogger("mylogger");
+ 
+
+            Parallel.For(0, 100, (i, loopState) =>
+            {
+                if (i >= 10)
+                    loopState.Break();
+                log.Debug(i);
+            });
+
+
+        }
+    }
+}
---
+++ CommenLogginDemo/LogDemo2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+
+namespace LogDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog log = LogManager.GetLogger("mylogger");
+            Parallel.For(0, 100, (i, loopState) =>
+            {
+                if (i > 10)
+                {
+                    loopState.Break();
+                    log.Debug($"异常数字{i}");
+                }
+                log.Debug($"正常数字{i}");
+            });
+
+
+            log.Debug("任务结束！！！");
+
+        }
+    }
+}
