0d9f9cd8828d9c1b2d6bb48a3bfc2a2d1a37fac7
chengongyi <chengongyi@email.com>
2018-08-16 18:01:39 +0800

    练习了Net Core中的配置操作

---
+++ NetCorePratice/SettingDemo001/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SettingDemo001
+{
+    class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+        static void Main(string[] args)
+        {
+            //配置文件可以通过文件传入
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json");
+
+            /*
+             * 通过ConfigurationBuilder 明确指定配置文件和路径。然后赋值给上下文
+             * 也就是说，我们可以在普通的类型项目中，通过这种方式，把配置文件加载到全局上下文中
+             * 
+             */
+            Configuration = builder.Build();
+
+            /*
+             * 取出数值的时候：
+             * 上下文分割：则使用：
+             * 取出索引：使用 数值去取
+             */
+            System.Console.WriteLine($"option1 GetSection:{Configuration.GetSection("Option1").Value}");
+            System.Console.WriteLine($"option1:{Configuration["Option1"]}");
+            System.Console.WriteLine($"suboption1:{Configuration["subsection:suboption1"]}");
+            System.Console.WriteLine($"Harry:17:{Configuration["wizards:1:Name"]}");
+            System.Console.WriteLine($"Harry:17:{Configuration["wizards:1:Age"]}");
+
+
+
+            ///配置信息可以通过字典数据传入
+            var dict = new Dictionary<string, string>
+            {
+                {"Profile:MachineName", "Rick"},
+                {"App:MainWindow:Height", "11"},
+                {"App:MainWindow:Width", "11"},
+                {"App:MainWindow:Top", "11"},
+                {"App:MainWindow:Left", "11"}
+            };
+
+            var builder1 = new ConfigurationBuilder();
+            builder1.AddInMemoryCollection(dict);
+            Configuration = builder1.Build();
+
+            //1.直接读取
+            System.Console.WriteLine($"Profile:MachineName:{Configuration["Profile:MachineName"]}");
+
+
+
+            //2.把数据提取到POCO类中
+            var window = new MyWindow();
+            Configuration.GetSection("App:MainWindow").Bind(window);
+            System.Console.WriteLine($"window.Left={window.Left}");
+
+            //3.使用GetValue,如果找不到这个值，还可以附上一个默认的值，这在开发时很有用
+            var left = Configuration.GetValue<int>("App:MainWindow:Left", 80);
+            Console.WriteLine($"Configuration.GetValue.Left {left}");
+
+
+
+        }
+        public class MyWindow
+        {
+            public int Height { get; set; }
+            public int Width { get; set; }
+            public int Top { get; set; }
+            public int Left { get; set; }
+        }
+    }
+}
---
+++ NetCorePratice/SettingDemo2/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace SettingDemo2
+{
+    public class AppSettings
+    {
+        public Window Window { get; set; }
+        public Connection Connection { get; set; }
+        public Profile Profile { get; set; }
+    }
+
+    public class Window
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+
+    public class Connection
+    {
+        public string Value { get; set; }
+    }
+
+    public class Profile
+    {
+        public string Machine { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            //1.使用绑定的方式，把Setting绑定到类中
+            var appConfig = new AppSettings(); 
+            config.GetSection("App").Bind(appConfig);
+
+
+            //2.直接从配置文件取出数据，并且加载到类中
+            var appConfig1 = config.GetSection("App").Get<AppSettings>();
+
+
+            Console.WriteLine(appConfig.Connection);
+            Console.WriteLine(appConfig1.Connection);
+
+
+        }
+    }
+}
